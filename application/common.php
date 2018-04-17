e3170d5868b49acda43b60ab8aa4b86e59194fdf
lucky <499820942@qq.com>
2018-04-17 17:47:35 +0800

    '添加正则验证公共方法'

--- application/common.php
+++ application/common.php
@@ -92,3 +92,39 @@ function getUuid(string $prefix='') : string
     $uuid .= substr($str,20);
     return strtoupper($prefix . $uuid);
 }
+
+
+
+/**
+ * pregVerify [正则验证]
+ *
+ * author dear
+ * @param $str : [要验证的字符串]
+ * @param $type : [指定类型]
+ * @return bool
+ */
+function pregVerify($str, $type){
+    $patterns = array(
+        'not_zn'	=>	'/[^\x{4e00}-\x{9fa5}]/u',		// 匹配非中文（UTF-8编码）
+        'zn'		=>	'/^\x{4e00}-\x{9fa5}+$/u',		// 匹配中文（UTF-8编码）
+        'not_int'	=>	'/[^\d]+?/',				// 匹配非整数数字
+        'p_int'		=>	'/^\d+?$/',					// 匹配正整数
+        'int'	   	=>  '/^[-\+]?\d+$/',			// 匹配整数
+        'double'    =>  '/^[-\+]?\d+(\.\d+)?$/',	// 匹配浮点数
+        'phone'		=>	'/^1\d{10}$/',			// 匹配手机
+        'email'		=>	'/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/',	// 匹配邮箱
+        'not_en'   	=>  '/[^A-Za-z]+?/',	// 匹配非英文
+        'en'   		=>  '/^[A-Za-z]+$/',	// 匹配英文
+        'bankcard'	=>	'/^(\d{16}|\d{18}|\d{19})$/',	// 银行卡号
+        'blank'		=>	'/\s+?/s',		// 单行模式 匹配空白字符
+        'common'	=>	'/[^0-9a-zA-Z]+?/', // 匹配非数字、字母、下划线
+        'common_zn'	=>	'/[^0-9a-zA-Z_\x{4e00}-\x{9fa5}]+?/u',	// 匹配非数字、字母、下划线、中文（UTF-8编码）
+        'url'       =>  "/^http(s?):\/\/(?:[A-za-z0-9-]+\.)+[A-za-z]{2,4}(:\d+)?(?:[\/\?#][\/=\?%\-&~`@[\]\':+!\.#\w]*)?$/"	// 匹配URL
+    );
+    if(!isset($patterns[$type])){
+        return false;
+    }
+    return preg_match($patterns[$type], $str) === 1;
+}
+
+
